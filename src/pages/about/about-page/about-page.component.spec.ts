104b80a7b7ab76d2aa767b723e04e3fb3505469a
betsy <betsyxqq@betsydeMacBook-Air.local>
2017-08-26 18:21:27 +0800

    commit

--- src/pages/about/about-page/about-page.component.spec.ts
+++ src/pages/about/about-page/about-page.component.spec.ts
@@ -23,3 +23,4 @@ describe('AboutPageComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
--- src/pages/about/about-page/about-page.component.ts
+++ src/pages/about/about-page/about-page.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 
+interface Account{
+  date:string,
+  name:string,
+  comment?:string,
+  budget?:number,
+  cost:number,
+  random?:number
+}
+
 @Component({
   selector: 'app-about-page',
   templateUrl: './about-page.component.html',
@@ -8,23 +17,62 @@ import { Meta, Title } from '@angular/platform-browser';
 })
 export class AboutPageComponent implements OnInit {
 
-    constructor(meta: Meta, title: Title) {
-    title.setTitle('My About Page');
-
-    meta.addTags([ 
-      {
-        name: 'author', content: 'anasit.com'
-      },
-      {
-        name: 'keywords', content: 'angular 4 tutorial, angular seo'
-      },
-      {
-        name: 'description', content: 'This is my great description.'
-      },
-    ])
+  accounting:Array<Account>;
+  constructor() {
+   this.getAccountingList()
   }
 
-  ngOnInit() {
-  }
+  
+  getAccountingList(){
+    this.accounting = [
+     {date:"2017/1/1" ,name:"Meal",cost:200,budget:500,random:20},
+     {date:"2017/1/1",name:"Traffic",cost:100,random:30},
+     {date:"2017/1/1",name:"Dress",cost:300,random:25},
+     {date:"2017/1/1",name:"cosmetics",cost:300,random:40},
+     {date:"2017/1/1",name:"Social",cost:500,random:15},
+     {date:"2017/1/1",name:"Electric Communication",cost:300},
+   ]
+ }
+
+ addAccount(){
+   let rrr = Math.random()
+   let newAccount= {date:"2017/1/3",name:"Traffic",cost:100,random: rrr}
+   this.accounting.push(newAccount)
+ }
+
+ asc(){
+   // 正序排列
+   // 数组操作API，https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+   this.accounting.sort((a,b)=>{
+     if(a.cost>b.cost){
+       return 1
+     }else{
+       return -1
+     }
+   })
+}
+
+
+ desc(){
+  this.accounting.sort((a,b)=>{
+    if(a.cost<b.cost){
+      return 1
+    }else{
+      return -1
+    }
+  })   
+ }
+ random(){
+   // 随机排列
+   // 常用数学计算API，https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+   this.accounting.sort((a,b)=>{
+      return Math.random()
+  
+  })
+ }
+ ngOnInit() {
+ } 
 
 }
+
